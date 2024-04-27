731cbe333b9723bec50001f5f92483eb303faefa
leofiore <leonardo.matias@gec.inatel.br>
2024-04-27 12:49:55 -0300

    :wrench: chore: config jest

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const config = {
+    testEnvironment: 'node',
+    transform: {
+        '.+\\.ts$': 'ts-jest'
+    }
+};
+exports.default = config;
